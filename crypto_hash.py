0a55ef5481e7065842b28319cb6ec7be30eca9c2
Haydens-workshop <76880991+Haydens-workshop@users.noreply.github.com>
2023-03-28 02:05:25 -0400

    Update crypto_hash.py

--- crypto_hash.py
+++ crypto_hash.py
@@ -1,25 +1,19 @@
 import hashlib
-import hmac
 import json
-import os
 
-def crypto_hash(*args, key=None):
+def crypto_hash(*args):
     """
-    Return a SHA-256 hash of given arguments using HMAC-SHA256.
+    Return a SHA-256 hash of the given arguments.
     """
-    if key is None:
-        key = os.environ.get('HMAC_SECRET_KEY')
-        if key is None:
-            raise ValueError("HMAC secret key not found.")
-    key = key.encode('utf-8')
+    stringified_args = sorted(map(lambda data: json.dumps(data), args))
 
-    message = json.dumps(args, sort_keys=True).encode('utf-8')
-    hmac_hash = hmac.new(key, message, hashlib.sha256).digest()
-    return hmac_hash.hex()
+    joined_data = ''.join(stringified_args)
+
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
     print(f"crypto_hash('one', 2, [3]): {crypto_hash('one', 2, [3])}")
-    print(f"crypto_hash('2', 'one', [3]): {crypto_hash('2', 'one', [3])}")
+    print(f"crypto_hash(2, 'one', [3]): {crypto_hash(2, 'one', [3])}")
 
 if __name__ == '__main__':
     main()
